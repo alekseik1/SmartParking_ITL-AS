44530a43219daf0a769b97cdc1b24163c614832a
alekseik1 <alekseik1@yandex.ru>
2015-12-17 17:35:16 +0300

    Закомментировал код с NFC, пока буду курить. Если вылетает приложения, откатитесь к этому коммиту и собирайте приложение)

--- SmartParking/src/com/tim/smartparking/OplataActivity.java
+++ SmartParking/src/com/tim/smartparking/OplataActivity.java
@@ -12,16 +12,28 @@ import java.text.SimpleDateFormat;
  */
 public class OplataActivity extends Activity {
 
+    static TextView tv3;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.oplata_info);
         NotificationManager nm = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
         nm.cancel(5);
+        //NfcAdapter adapter = NfcAdapter.getDefaultAdapter(this);
         TextView tv1 = (TextView) findViewById(R.id.textView);
         TextView tv2 = (TextView) findViewById(R.id.textView7);
-        TextView tv3 = (TextView) findViewById(R.id.textView8);
         SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
-        String stayTime = sdf.format(System.currentTimeMillis() - ServerTest.parkTime - 3600 * 1000 * 3);
-        tv1.setText(stayTime);
+        long time = System.currentTimeMillis() - ServerTest.parkTime - 3600 * 1000 * 3;
+        String stayTime = sdf.format(time);
+        sdf = new SimpleDateFormat("HH");
+        tv1.setText("Время стоянки: " + stayTime);
+        if (Integer.parseInt(sdf.format(time)) < 1) {
+            tv2.setText("Первый час на парковке бесплатен!");
+            tv3.setText("");
+        } else {
+            tv2.setText("Стоимость: " + Integer.parseInt(sdf.format(time)) * 100);
+        }
+        tv2.setText("Стоимость: " + Integer.parseInt(sdf.format(time)) * 100);
+
     }
+
 }
